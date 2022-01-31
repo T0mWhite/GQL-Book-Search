6209004dce0426b0809fbd9c9d3a3e4664ef1383
T0mWhite <tbw2.mail@gmail.com>
2022-01-31 12:50:27 -0700

    Heroku push again

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: Book!) {
+    addBook(book: $book) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+      }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($savedBooks: String!) {
+    addBook(savedBooks: $savedbooks) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+      }
+    }
+`;
